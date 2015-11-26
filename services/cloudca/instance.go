2870115038e8f472c2e1c23e4454d6de5334c44c
Franz Garcia <franzpgarcia@gmail.com>
2015-11-26 12:06:56 -0500

    Added extra methods to InstanceService

--- services/cloudca/instance.go
+++ services/cloudca/instance.go
@@ -8,7 +8,9 @@ type InstanceService interface {
 	Get(id string) (Instance, error)
 	GetByName(name string) (Instance, error)
 	List() ([]Instance, error)
-	Create(Instance)
+	Create(Instance) (bool, error)
+	Delete(id string, purge bool) (bool, error)
+	Exists(id string) (bool, error)
 }
 
 type InstanceApi struct {
@@ -29,12 +31,22 @@ func (instanceApi InstanceApi) GetByName(name string) (Instance, error) {
 	return Instance{}, nil
 }
 
-
 func (instanceApi InstanceApi) GetByName(name string) (Instance, error) {
 	return Instance{}, nil
 }
 
-
 func (instanceApi InstanceApi) List() ([]Instance, error) {
 	return []Instance{}, nil
+}
+
+func (instanceApi InstanceApi) Create(instance Instance) (bool, error) {
+	return false, nil
+}
+
+func (instanceApi InstanceApi) Delete(id string, purge bool) (bool, error) {
+	return false, nil
+}
+
+func (instanceApi InstanceApi) Exists(id string) (bool, error) {
+	return false, nil
 }
