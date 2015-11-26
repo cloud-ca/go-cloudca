2fcd9b2045cc744e4968aacbb33c43e0819df57c
Franz Garcia <franzpgarcia@gmail.com>
2015-11-26 13:57:27 -0500

    Added Template struct

--- services/cloudca/entity_type.go
+++ services/cloudca/entity_type.go
@@ -2,6 +2,7 @@ package services/cloudca
 
 const (
 	INSTANCE_ENTITY_TYPE = "instances"
+	TEMPLATE_ENTITY_TYPE = "templates"
 	TIER_ENTITY_TYPE = "tiers"
 	COMPUTE_OFFERING_ENTITY_TYPE = "computeofferings"
 	DISK_OFFERING_ENTITY_TYPE = "diskofferings"
---
+++ services/cloudca/template.go
@@ -0,0 +1,33 @@
+package services/cloudca
+
+type Template struct {
+
+}
+
+type TemplateService interface {
+	Get(id string) (Template, error)
+	GetByName(name string) (Template, error)
+	List() ([]Template, error)
+}
+
+type TemplateApi struct {
+	entityService EntityService
+}
+
+func NewInstanceService(apiClient CCAApiClient, serviceCode string, environmentName string) TemplateService {
+	return TemplateApi{
+		"entityService": NewEntityService(apiClient, serviceCode, environmentName, TEMPLATE_ENTITY_TYPE)
+	}
+}
+
+func (templateApi TemplateApi) Get(id string) (Template, error) {
+	return Template{}, nil
+}
+
+func (templateApi TemplateApi) GetByName(name string) (Template, error) {
+	return Template{}, nil
+}
+
+func (templateApi] TemplateApi) List() ([]Template, error) {
+	return []Template{}, nil
+}
