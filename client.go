d3a129421c7ef52fa15f67cce8a450138ab36644
Franz Garcia <franzpgarcia@gmail.com>
2015-11-26 11:49:51 -0500

    Fixed NewCCAClient function

--- client.go
+++ client.go
@@ -6,19 +6,20 @@ const (
 )
 
 type CCAClient struct {
+	apiURL string
+	apiKey string
 	apiClient CCAApiClient
 	Tasks TaskService
 }
 
 func NewCCAClient(apiKey string) CCAClient {
-	ccaClient := CCAClient{}
-	ccaClient.apiClient = CCAApiClient{DEFAULT_API_URL, apiKey}
-	ccaClient.Tasks = TaskApi{ccaClient.apiClient}
-	return ccaClient
+	return NewCCAClientWithCustomURL(DEFAULT_API_URL, apiKey)
 }
 
 func NewCCAClientWithCustomURL(apiURL string, apiKey string) CCAClient {
 	ccaClient := CCAClient{}
+	ccaClient.apiURL = apiURL
+	ccaClient.apiKey = apiKey
 	ccaClient.apiClient = CCAApiClient{apiURL, apiKey}
 	ccaClient.Tasks = TaskApi{ccaClient.apiClient}
 	return ccaClient
@@ -28,4 +29,12 @@ func NewCCAClientWithCustomURL(apiURL string, apiKey string) CCAClient {
 //For now it assumes that the serviceCode belongs to a cloud.ca service
 func (c CCAClient) GetResources(serviceCode string, environmentName string) {
 
+}
+
+func (c CCAClient) GetApiURL() string {
+	return c.apiURL
+}
+
+func (c CCAClient) GetApiKey() string {
+	return c.apiKey
 }
